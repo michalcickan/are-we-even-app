2e5badc6fa80b7742e557f3411d1392e6b93cd0e
Michal Cickan <michalcickan@icloud.com>
2023-07-07 17:05:37 +0200

    add .gitignore

---
+++ lib/env/env.dart
@@ -0,0 +1,10 @@
+// lib/env/env.dart
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied()
+abstract class Env {
+  @EnviedField(varName: 'GOOGLE_CLIENT_ID')
+  static const String googleClientId = _Env.googleClientId;
+}
